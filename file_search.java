ef6efc486df681f178c097977da7373546a90c60
Igor <igoru@ukr.net>
2011-08-04 14:42:35 +0300

    test commit

--- file_search.java
+++ file_search.java
@@ -31,7 +31,11 @@ public void find (File search_folder)
 				if ((folder_list[file_index].isDirectory())&& (!folder_list[file_index].getName().equals("System Volume Information")) && (!folder_list[file_index].getName().equals(".."))&& (!folder_list[file_index].getName().equals(".")))
 				{
 					//Oleh: Why do you need this system.out?
-					System.out.println(folder_list[file_index].getPath());
+					/*Igor: In origin code I did not this output. But during searching in huge folder
+					for example in C:\ it is not clear "Ip the program work correct or some stuck happened.
+					Therefore I set hear this redundant out. Fixed.
+					*/
+					//System.out.println(folder_list[file_index].getPath());
 					find(folder_list[file_index]);
 				}
 					
