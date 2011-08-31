b94088a2cf69690464f913e2fbfb6002bcb43838
Igor <igoru@ukr.net>
2011-08-31 09:36:40 +0300

    fixes after third review

--- FileFinder.java
+++ FileFinder.java
@@ -2,95 +2,76 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.FileFilter;
 import java.util.*;
-
-//Oleh: it is usually better to name classes as nouns. In this case something like "FileFinder" suits best.
-//Igor: Done
+/*
+ List of changes after third review:
+ 1.Public members changed to Private. Added Public function to work with private members.
+ 2. Iteration by HASH MAP changed to For in against using iterator
+ 3. Collection instances are using now generics
+ 4. Manual string reconstruction changed to String.format 
+ 5. Variables now are declared with more basis classes(Set against TreeSet,... )
+   
+ */
 public class FileFinder {
-	//Initialization collection with found files 
-	//class to filter required files
-
-//Oleh: maybe better to name filenameFilter
-//Igor: Done
-	private FilenameFilter filenameFilter;
-
-//Oleh: "isFolder is a good name for the boolean flag. in this case it would be better to name it folderFilter
-//Igor: Done
-	private FileFilter folderFilter;
-	//Initialization collection with list of searching files
-
-//Oleh: We will discuss generics soon. This is ok for now.
-	//private TreeSet foundFiles = new TreeSet();
-	private HashMap foundFiles = new HashMap();
-	public TreeSet searchingFiles = new TreeSet();
-	
-
-//Oleh: "ignore folders" is imperative voice. "IGNORED_FOLDERS" is a noun which is better.
-//Igor: Done
-	private final static TreeSet IGNORED_FOLDERS = new TreeSet();
-
-//Oleh: static initializer. good.
+	private final static TreeSet<String> IGNORED_FOLDERS = new TreeSet<String>();
     static {
     	IGNORED_FOLDERS.add("System Volume Information");
     	IGNORED_FOLDERS.add(".");
     	IGNORED_FOLDERS.add("..");
 	}
-
-	public void findFiles (File rootFolder)	{
-//Oleh: camel case searchFolder (or maybe roorFolder?)
-//Igor: Done
+	private FilenameFilter filenameFilter;
+	private FileFilter folderFilter;
+	private Map<File, Set<File>> foundFiles = new HashMap<File, Set<File>>();
+	private Set<String> searchingFiles = new TreeSet<String>();
+	private Set<File> foundFileSet;
+	public void setSearchingFiles(String fileName)
+	{
 		
-		
-		filenameFilter = new FileFilterer(searchingFiles);
-		folderFilter = new FolderFilter(IGNORED_FOLDERS);
-		File[] fileList = rootFolder.listFiles(filenameFilter);
-		File[] folderList = rootFolder.listFiles(folderFilter);
-		TreeSet fileSet = new TreeSet();
-//Oleh: for (File file : filelList) {
-//Igor: Fixed
-		//for (File file : fileList) {
-		fileSet.addAll(Arrays.asList(fileList));
-		foundFiles.put(rootFolder, fileSet);
-		//}
-
-//Oleh: for (File directory : folderList) {
-//Igor: Fixed				
-		for (File directory : folderList) {
-			findFiles(directory);
-					
+		if (!fileName.trim().isEmpty()) {
+				searchingFiles.add(fileName);
+		} else {
+			System.out.println("File Name can't be None!!!");
 		}
 		
 	}
+	public void findFiles (File rootFolder)	{
+		filenameFilter = new FileFilterer(searchingFiles);
+		folderFilter = new FolderFilter(IGNORED_FOLDERS);
+		File[] fileList = rootFolder.listFiles(filenameFilter);
+		foundFileSet = new TreeSet<File>();
+		foundFileSet.addAll(Arrays.asList(fileList));
+		if (!foundFileSet.isEmpty()) {
+			foundFiles.put(rootFolder, foundFileSet);
+		}
+		File[] folderList = rootFolder.listFiles(folderFilter);
+		for (File directory : folderList) {
+			findFiles(directory);
+		}
+	}
 		public void printResults ()	{
-		TreeSet filesSet = new TreeSet();
-		Set fileKeys = foundFiles.entrySet();
-		Iterator iter =  fileKeys.iterator();
-		while (iter.hasNext())	{
-			Map.Entry filemapEntry = (Map.Entry)iter.next();
-			System.out.println("In folder \""+filemapEntry.getKey()+ "\" found file(s):");
-			filesSet=(TreeSet)filemapEntry.getValue();
-			//System.out.println(filemapEntry.getValue());
+		Set<File> filesSet = new TreeSet<File>();
+		Set<File> fileKeys = foundFiles.keySet();
+		for (File baseFolder : fileKeys)
+		{
+			System.out.println(String.format("In folder \"%s\" found file(s):", baseFolder));
+			filesSet=foundFiles.get(baseFolder);
 			for(Object fileObj :filesSet){
 				File file = (File)fileObj;
 				System.out.println(file.getName());
-				
-			}
-					
+		}
 		}
 	  }
-		/* Implementation of filter class. This class is used to filter required files
-		 * in current folder*/
-class FileFilterer implements FilenameFilter{
-	private TreeSet fileNames;
-	public FileFilterer(TreeSet fileNames){
+ private class FileFilterer implements FilenameFilter{
+	private Set<String> fileNames;
+	public FileFilterer(Set<String> fileNames){
 		this.fileNames = fileNames;
 		}
 	public boolean accept(File directory, String filename) {
 			 return fileNames.contains(filename);
 			 }
 		}
-class FolderFilter implements FileFilter{
-	private TreeSet fileNames;
-	public FolderFilter(TreeSet fileNames){
+private class FolderFilter implements FileFilter{
+	private Set<String> fileNames;
+	public FolderFilter(Set<String> fileNames){
 				this.fileNames = fileNames;
 		}
 	
@@ -100,6 +81,8 @@ class FolderFilter implements FileFilter{
 		}else { 
 				return false;
 			   }
+		//Encountered with problem to return in one string
+		//return !fileNames.contains(fileName.getName())||fileName.isDirectory();
 				
 	}
 }		
